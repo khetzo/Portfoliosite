98e954bad5ad4345ab19dc2c571909075585bb34
khetzo <khetzo50@gmail.com>
2025-08-09 20:19:46 +0200

    removing unvwanted comment

--- docs/script.js
+++ docs/script.js
@@ -14,21 +14,21 @@ document.addEventListener('DOMContentLoaded', function () {
   const searchAppearances = 1;
 
   button.addEventListener('click', function () {
-    // Show the popup
+    
     popup.style.display = 'flex';
 
     // Delay the actions by 2 seconds
     setTimeout(function () {
-      // Phone call
+     
       window.location.href = 'tel:+27767786789';
 
-      // WhatsApp message
+   
       window.location.href = 'https://wa.me/27767786789';
       popup.style.display = 'none';
-    }, 2000); // 2000 milliseconds = 2 seconds
+    }, 2000); 
   });
 
-  // Hide the popup when clicking outside
+  
   window.addEventListener('click', function (event) {
     if (event.target === popup) {
       popup.style.display = 'none';
